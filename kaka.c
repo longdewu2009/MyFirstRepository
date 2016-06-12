689ee6b2a5acf107acad0c5420bc438b2ef3f638
longdw0307 <longdw0307@thundersoft.com>
2016-06-12 16:26:48 +0800

    Up to kaka.c

---
+++ kaka.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: kaka.c
+	> Author: longdw
+  > mail: longdw0307@thundersoft.com
+	> Created Time: 2016年06月12日 星期日 16时25分38秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+
+int main(int argc. char** argv)
+{
+	return 0;
+}
