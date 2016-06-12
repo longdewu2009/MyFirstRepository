a1c870228426d497ee5f2c4cf314a677263a1e7d
longdw0307 <longdw0307@thundersoft.com>
2016-06-12 16:51:44 +0800

    There is  three fules

---
+++ owen.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: owen.c
+	> Author: longdw
+  > mail: longdw0307@thundersoft.com
+	> Created Time: 2016年06月12日 星期日 16时50分10秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main(int agrc, char** argv)
+{
+	printf("dffffff\n");
+	return 0;
+}
